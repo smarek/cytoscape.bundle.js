a3aa15f0cc2c11fdb162d71e0e67b5b205f7fa6b
Marek Sebera <marek.sebera@gmail.com>
2021-01-19 20:42:19 +0100

    esbuild working

--- app.jsx
+++ app.jsx
@@ -1,12 +1,13 @@
-import * as cytoscape_core from 'cytoscape'
-import * as layout_cise from 'cytoscape-cise'
-import * as layout_dagre from 'cytoscape-dagre'
-import * as layout_cola from 'cytoscape-cola'
-import * as layout_klay from 'cytoscape-klay'
+import cytoscape from 'cytoscape'
+import cise from 'cytoscape-cise'
+import dagre from 'cytoscape-dagre'
+import cola from 'cytoscape-cola'
+import klay from 'cytoscape-klay'
 
-cytoscape.use(layout_cise, 'cise')
-cytoscape.use(layout_dagre, 'dagre')
-cytoscape.use(layout_cola, 'cola')
-cytoscape.use(layout_klay, 'klay')
 
-globalThis.cytoscape = cytoscape_core.default
+globalThis.cytoscape = cytoscape
+
+cytoscape.use(cise)
+cytoscape.use(dagre)
+cytoscape.use(cola)
+cytoscape.use(klay)
